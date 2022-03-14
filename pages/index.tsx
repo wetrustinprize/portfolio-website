146956903bc937b3d601c79f389cce5b213ae4c4
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-14 19:42:55 -0300

    starting making index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from "next";
-import Header from "../components/Header";
+import { AnimText } from "../components/AnimText";
+import Layout from "../components/Layout";
+import PurpleDiv from "../components/PurpleDiv";
 
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <Layout>
+      <PurpleDiv>
+        <h1>
+          Hello! I'm <AnimText>Peterson Adami</AnimText>.
+        </h1>
+        <h2>I'm a fullstack developer.</h2>
+      </PurpleDiv>
+    </Layout>
+  );
 };
 
 export default Home;
