9f814c6f79b98d8ad4d2a302023b3bfa4387f347
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-15 19:43:33 -0300

    animation on route change

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from "../Header";
+import { AnimatePresence, motion } from "framer-motion";
 
 import style from "./styles.module.scss";
 
 const Layout: React.FC = ({ children }) => {
+  const animVariants = {
+    hidden: { opacity: 0 },
+    enter: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+
   return (
     <>
       <Header />
-      <main className={style.container}>{children}</main>
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <motion.main
+          variants={animVariants}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          className={style.container}
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
     </>
   );
 };
