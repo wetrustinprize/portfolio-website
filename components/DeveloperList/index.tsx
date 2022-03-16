85b1a9f691e18ecdca293d73f3e6443d07f8f183
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-15 21:35:45 -0300

    developer skills component

---
+++ components/DeveloperList/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import PurpleDiv from "../PurpleDiv";
+
+import style from "./style.module.scss";
+
+import { BsSuitHeartFill } from "react-icons/bs";
+
+export interface DevTool {
+  icon: React.ReactNode;
+  name: string;
+  rate: number;
+  favorite?: boolean;
+}
+
+export interface IDeveloperList {
+  title: {
+    prefix: string;
+    sufix: string;
+  };
+  devTools: DevTool[];
+}
+
+const DeveloperList: React.FC<IDeveloperList> = ({ title, devTools }) => {
+  return (
+    <PurpleDiv className={style.container}>
+      <h1 className={style.title}>
+        {title.prefix} <b>{title.sufix}</b>
+      </h1>
+      <div className={style.devContainer}>
+        {devTools
+          .sort((a, b) => b.rate - a.rate)
+          .map((devTool, index) => (
+            <div className={style.dev} key={index}>
+              {devTool.icon}
+              {devTool.favorite && (
+                <BsSuitHeartFill className={style.favorite} />
+              )}
+              <h1 className={style.devTitle}>{devTool.name}</h1>
+              <div className={style.rateContainer}>
+                {[...Array(4).keys()].map((_, index) => (
+                  <div
+                    key={index}
+                    className={
+                      index < devTool.rate ? style.rate : style.rateEmpty
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </PurpleDiv>
+  );
+};
+
+export default DeveloperList;
