8060527c878b4458f4ed73eb3b9f3c631a2df233
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-16 19:43:58 -0300

    my projects as const

---
+++ data/projects.ts
@@ -0,0 +1,19 @@
+import { Project } from "../components/Project";
+
+const PROJECTS: Project[] = [
+  {
+    title: "Zork",
+    description:
+      "Zork is a simple service to trade points with your friends, or ask points from them.",
+    image:
+      "https://github.com/wetrustinprize/zork-frontend/raw/master/screenshots/index.png",
+    buttons: [
+      {
+        text: "Github",
+        link: "https://github.com/wetrustinprize/zork-frontend",
+      },
+    ],
+  },
+];
+
+export default PROJECTS;
