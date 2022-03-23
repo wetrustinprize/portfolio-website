357b5ba1b7520ffc2a6e686cc888604fbb2be9f1
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-23 20:55:41 -0300

    projects page

---
+++ pages/projects/index.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import React from "react";
+import AnimDiv from "../../components/AnimDiv";
+import Button from "../../components/Button";
+import Layout from "../../components/Layout";
+import Project from "../../components/Project";
+import PROJECTS from "../../data/projects";
+
+import styles from "./styles.module.scss";
+
+const Projects: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <AnimDiv className={styles.container} delayPerChild={0.05} duration={0.5}>
+        {PROJECTS.map((project, index) => (
+          <Project key={index} project={project} />
+        ))}
+        <Button
+          className={styles.button}
+          text="More in my Github"
+          onClick={() => router.push("https://github.com/wetrustinprize")}
+        />
+      </AnimDiv>
+    </Layout>
+  );
+};
+
+export default Projects;
