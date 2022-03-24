68cae231cab6f9ce0c3e1992cb73ca9a5fd6781e
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-23 21:48:52 -0300

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+import styles from "./404.module.scss";
+
+const messages = [
+  "What where you looking for?",
+  "This page doesn't exist",
+  "Nothing to see here",
+  "Nothing hidden here...",
+];
+
+const FourOhFour: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <h2 className={styles.subtitle}>
+        {messages[(Math.random() * messages.length) | 0]}
+      </h2>
+      <Link passHref href={"/"}>
+        <p className={styles.return}>return home</p>
+      </Link>
+    </div>
+  );
+};
+
+export default FourOhFour;
