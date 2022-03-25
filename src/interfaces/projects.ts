6959845d2ab69f9df413928eb2a182ba3b6e5531
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-25 12:55:11 -0300

    interface folder

---
+++ src/interfaces/projects.ts
@@ -0,0 +1,12 @@
+export default interface Project {
+  title: string;
+  description: string;
+  image: string;
+  tags?: string[];
+  buttons?: [
+    {
+      text: string;
+      link: string;
+    }
+  ];
+}
