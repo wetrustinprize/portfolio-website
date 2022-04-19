0064e6ab4b41c0ebbc64923d767ab15e5a3a6234
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-04-19 19:23:59 -0300

    new project: Distancier! :tada:

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,7 +13,25 @@ const PROJECTS: Project[] = [
         link: "https://github.com/wetrustinprize/zork-frontend",
       },
     ],
-    tags: ["React", "Express"],
+    tags: ["Next.js", "Express"],
+  },
+  {
+    title: "Distancier",
+    description:
+      "Simple web app to pick a new house, and check which one is better by checking the distance between places.",
+    image:
+      "https://github.com/wetrustinprize/distancier/raw/main/screenshots/details.png",
+    buttons: [
+      {
+        text: "Github",
+        link: "https://github.com/wetrustinprize/distancier",
+      },
+      {
+        text: "Demo",
+        link: "https://distancier.wetrustinprize.com",
+      },
+    ],
+    tags: ["Next.js", "Google API"],
   },
   {
     title: "Q1nfluencer",
